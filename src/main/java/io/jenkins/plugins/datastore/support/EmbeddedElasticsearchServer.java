9cc92ed3327996b0b85c530a6f8684aaca4129a7
Michael McCaskill <mmccaskill@cloudbees.com>
2016-07-22 18:00:25 -0400

    Simplify indexing process
    It's unscable to index at startup along with making ~1.2k HTTP calls to
    get stats plus possibly another ~1.2k HTTP calls to scrape HTML content.
    The plan moving forward is to rely on the build process to generate JSON
    file(s) that include all pertinent information so that all that has to
    happen at startup is the app reads these files and performs simple
    indexing.
    
    In the future these files shouldn't be bundled with the WAR but hosted
    on S3 thereby making it possible to freshen the data without having to
    rebuild the app. Ideally we'll have a REST endpoint that tells the app
    to reindex using the file from S3.

--- src/main/java/io/jenkins/plugins/datastore/support/EmbeddedElasticsearchServer.java
+++ src/main/java/io/jenkins/plugins/datastore/support/EmbeddedElasticsearchServer.java
@@ -1,11 +1,14 @@
 package io.jenkins.plugins.datastore.support;
 
 import org.apache.commons.io.FileUtils;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +17,8 @@ import javax.annotation.PreDestroy;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Copied & modified from:
@@ -45,7 +50,7 @@ public class EmbeddedElasticsearchServer {
       .build();
     node = NodeBuilder.nodeBuilder().local(true).settings(settings).build();
     node.start();
-    createIndex();
+    createAndPopulateIndex();
     logger.info("Initializing elasticsearch done");
   }
 
@@ -55,19 +60,34 @@ public class EmbeddedElasticsearchServer {
     FileUtils.deleteQuietly(this.dataDir);
   }
 
-  public void createIndex() {
+  private void createAndPopulateIndex() {
+    final ClassLoader cl = getClass().getClassLoader();
+    final String index = String.format("plugins_%s", DateTimeFormatter.ofPattern("yyyy.mm.dd_HH.mm.ss").format(LocalDateTime.now()));
     try {
-      final ClassLoader cl = getClass().getClassLoader();
-      final File file = new File(cl.getResource("elasticsearch/plugins.json").getFile());
-      final String body = FileUtils.readFileToString(file, "utf-8");
+      final File mappingFile = new File(cl.getResource("elasticsearch/mappings/plugins.json").getFile());
+      final String mappingContent = FileUtils.readFileToString(mappingFile, "utf-8");
       final Client client = getClient();
-      client.admin().indices().prepareCreate("plugins")
-        .addMapping("plugins", body)
+      client.admin().indices().prepareCreate(index)
+        .addMapping("plugins", mappingContent)
         .get();
+      logger.info(String.format("Index '%s' created", index));
+      final File dataFile = new File(cl.getResource("elasticsearch/data/plugins.json").getFile());
+      final String data = FileUtils.readFileToString(dataFile, "utf-8");
+      final JSONObject json = new JSONObject(data);
+      final BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
+      json.keySet().stream().forEach((key) -> {
+        final JSONObject plugin = json.getJSONObject(key);
+        final IndexRequest indexRequest = client.prepareIndex(index, "plugins", key).setSource(plugin.toString()).request();
+        bulkRequestBuilder.add(indexRequest);
+      });
+      client.bulk(bulkRequestBuilder.request()).get();
+      logger.info(String.format("Indexed %d plugins", json.keySet().size()));
+      client.admin().indices().prepareAliases().addAlias(index, "plugins").get();
+      logger.info(String.format("Alias plugins points to index %s", index));
     } catch (Exception e) {
-      logger.error("Problem creating index", e);
+      logger.error("Problem creating and populating index", e);
+      throw new RuntimeException("Problem creating and populating index", e);
     }
-
   }
 
 }
